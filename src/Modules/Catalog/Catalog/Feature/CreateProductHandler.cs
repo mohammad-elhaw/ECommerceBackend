ac867670276bc2377b3ba572dee83fb9a8461d2d
Mohammad Elhaw <mohammadelhaw2@gmail.com>
2025-09-15 11:46:54 +0300

    adding CQRS abstractions for command and queries

---
+++ src/Modules/Catalog/Catalog/Feature/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using Shared.CQRS;
+
+namespace Catalog.Feature;
+
+public record CreateProductCommand(
+    string Name, List<string> Categories, string Description,
+    string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid ProductId);
+
+public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Shared/Shared/CQRS/IQeuryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Shared.CQRS;
+public interface IQeuryHandler<in TQuery, TResponse> 
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
